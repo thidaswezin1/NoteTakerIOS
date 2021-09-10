c8e6f67029796d1a81bbdda17a6b2a8f55ed3917
thida <isgmisgm1234>
2021-09-10 13:49:38 +0630

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by ISGM on 10/09/2021.
+//  Copyright © 2021 thida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
